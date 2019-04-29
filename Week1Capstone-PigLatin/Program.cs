e8fa5ceab77c13e1334bf9d71f4707f2750e658a
Nick <nick.portell12@gmail.com>
2019-04-28 22:54:27 -0400

    This is my second commit. Made sure program could convert words with more than just one consonant before first vowel.

--- Week1Capstone-PigLatin/Program.cs
+++ Week1Capstone-PigLatin/Program.cs
@@ -36,12 +36,39 @@ namespace Week1Capstone_PigLatin
               for each word in the line.
              */
             //string.substring(index of string, literal length)
+            string response = "y";
+            string word;
+            
 
-            Console.Write("Please enter a word: ");
-            string word = Console.ReadLine();
+            Console.WriteLine("Welcome to the Pig Latin Translator!");
+
+            while (response == "y")
+            {
+                Console.Write("\nEnter a line to be translated: ");
+                word = Console.ReadLine();
+
+                //checks if user input was in all caps
+                if (word == word.ToUpper())
+                {
+                    Console.WriteLine("\n" + (ToPigLatin(word)).ToUpper());
+                }
+                //while not all caps, checks to see if atleast the first letter is uppercase
+                else if (word[0] == (word.ToUpper())[0])
+                {
+                    Console.WriteLine("\n" + ToTitle(ToPigLatin(word)));
+                }
+                //checks to see if any caps
+                else
+                {
+                    Console.WriteLine("\n" + (ToPigLatin(word)).ToLower());
+                }
+
+                Console.Write("\nTranslate another line? (y/n): ");
+                response = Console.ReadLine();
 
 
-            Console.WriteLine($"{word} is now {ToPigLatin(word)}");
+            }
+
             
         }
         //Convert to PigLatin
@@ -51,53 +78,54 @@ namespace Week1Capstone_PigLatin
             int count = 0;
             string consonants, word;
 
-
             //does something whenever it sees a vowel
 
             if (message.Contains("a") || message.Contains("e") || message.Contains("i") || message.Contains("o") || message.Contains("u") )
             {
+                //if first letter is a vowel, adds way to message and returns it
                 if (message[0] == 'a' || message[0] == 'e' || message[0] == 'i' || message[0] == 'o' || message[0] == 'u')
                 {
                     return message += "way";
                 }
-                //finds first vowel and moves consonants before it to the end of the word sequentially
-                else
+                else //if the first letter is a consonant
                 {
-                    for(int i =0; i < message.Length ;i++)
+                    //finds first vowel and moves consonants in their initial order to the end of the word sequentially
+                    for (int i =0; i < message.Length ;i++)
                     {
-
-                        if (message[i] != 'a' && message[i] != 'e' && message[i] != 'i' && message[i] != 'o' && message[i] != 'u')
+                        //if letter is not a vowel, record the number of characters scanned
+                        if (message[i] != 'a' && message[i] != 'e' && message[i] != 'i' && message[i] != 'o' && message[i] != 'u' )
                         {
                             count++;
                         }
-                        else
+                        else //if current letter is a vowel, add the substring of consonants that precede it, to the end and then add "ay"
                         {
                             consonants = message.Substring(0, count);
-                            word = message.Substring(count, message.Length - 1) + consonants + "ay";
+                            word = message.Substring(count, message.Length - count) + consonants + "ay";
                             return word;
                         }
                         
                     }
-                    return message;
+                    return "Your word does not start with either a vowel or consonant.";
                 }
             }
-            else
+            else //user input doesnt contain a vowel 
             {
                 return message + "ay";
             }
 
         }
-
-        public static int StrLength(string message)
+        //Convert changed word from PigLatin to TitleCase-PigLatin
+        public static string ToTitle(string word)
         {
-            int stringLength = 0;
+            string firstLetter = (word.Substring(0,1)).ToUpper();
+            string restOfWord = (word.Substring(1, word.Length - 1)).ToLower();
+            string result = firstLetter + restOfWord;
 
-            foreach (char letter in message)
-            {
-                stringLength++;
-            }
-
-            return stringLength;
+            return result;
         }
+            
+        
+
     }
+
 }
